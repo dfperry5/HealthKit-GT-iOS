e4efbfaecf346e944be01d437473915e42648dc7
DylanPerry5@gmail.com <DylanPerry5@gmail.com>
2019-11-15 17:44:42 -0500

    Initial Commit

---
+++ HealthKitSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthKitSample
+//
+//  Created by Dylan Perry on 11/15/19.
+//  Copyright © 2019 Dylan Perry. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
